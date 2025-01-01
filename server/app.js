e417410c9b87e70bfe638d9a76f75d5fd3ae0f84
Vivek Kumar <kumarvivek7282827749@gmail.com>
2025-01-01 20:24:53 +0530

    Added few things for deployment

--- server/app.js
+++ server/app.js
@@ -6,19 +6,22 @@ import productRoutes from './routes/productRoute.js';
 import cartRoutes from './routes/cartRoute.js';
 import orderRoutes from './routes/orderRoute.js';
 import wishlistRoutes from './routes/wishlistRoute.js';
-
+import path from 'path';
+import { fileURLToPath } from "url";
 const app = express();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // Middleware
 app.use(express.json()); // Handles JSON payloads
 app.use(express.urlencoded({ extended: true })); // Handles form submissions
 
 // Configure CORS: Allow specific origin, methods, and headers
 app.use(cors({
-  origin: 'http://localhost:5173', // Allow requests from your frontend
+  origin: ['http://localhost:5173','*'], // Allow requests from your frontend
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Allow these HTTP methods
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'], // Allow these headers including Authorization
 }));
+
 
 // Logging middleware to log incoming requests
 app.use((req, res, next) => {
@@ -40,6 +43,10 @@ app.use('/api/auth',wishlistRoutes);
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 export default app;
