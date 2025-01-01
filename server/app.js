ad66a73dcac565bd4f6a25fe542f34495919aef0
Vivek Kumar <kumarvivek7282827749@gmail.com>
2025-01-01 22:27:52 +0530

    added public ip to access the server publically

--- server/app.js
+++ server/app.js
@@ -41,7 +41,7 @@ app.use('/api/auth',wishlistRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 app.use(express.static(path.join(__dirname, "../client/dist")));
